4a18994ffea8f4a5ef64b6ee52908af2db8cf1ad
Simon Leigh <sleigh.dev@gmail.com>
2019-09-06 18:28:36 +0900

    PouchDBCore no leveldown, fix node crash

--- api/db.ts
+++ api/db.ts
@@ -1,6 +1,34 @@
+import ExpressPouch from "express-pouchdb"
 import { NowRequest, NowResponse } from '@now/node'
+import express from "express"
+import cors from "cors"
+import PouchDBCore from 'pouchdb-core'
+import PouchDBMemoryAdapter from 'pouchdb-adapter-memory'
+import PouchDBMapReduce from 'pouchdb-mapreduce'
+import PouchDBHttpAdapter from 'pouchdb-adapter-http'
+import PouchDBReplication from 'pouchdb-replication'
+import PouchDBFind from 'pouchdb-find'
+
+const PouchDB = PouchDBCore
+  .plugin(PouchDBMemoryAdapter)
+  .plugin(PouchDBHttpAdapter)
+  .plugin(PouchDBMapReduce)
+  .plugin(PouchDBReplication)
+  .plugin(PouchDBFind)
+
+PouchDB.defaults({
+  adapter: "memory"
+})
+
+const app = express()
+app.use(cors({
+  credentials: true
+}))
+app.use((req, res, next) => {
+  console.log(req.params)
+  return ExpressPouch(PouchDB)(req, res)
+})
 
 export default (request: NowRequest, response: NowResponse) => {
-  
-  return response.json(request.query)
+  return app(request, response)
 }
